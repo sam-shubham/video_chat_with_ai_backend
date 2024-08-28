405b9278b8c2315bfb7470804ea992e5f5f4faaf
sam-shubham <mrshubhamsamrat05@gmail.com>
2024-08-28 12:49:29 +0530

    trash removed

--- pages/api/ScrapWebsite-old/querySementicSearchPinecone.js
+++ pages/api/ScrapWebsite-old/querySementicSearchPinecone.js
@@ -1,29 +1,29 @@
-import dbConnect from "@/libs/database/dbconnect";
-import openAiSettings from "@/libs/database/models/openAiSettings";
-import {
-  callVectorDBQAChain,
-  deleteVectors,
-  getAllVectors,
-} from "@/libs/vectorDataSetsPinecone/vectorStore";
-import { createRouter, expressWrapper } from "next-connect";
-const router = createRouter();
+// import dbConnect from "@/libs/database/dbconnect";
+// import openAiSettings from "@/libs/database/models/openAiSettings";
+// import {
+//   callVectorDBQAChain,
+//   deleteVectors,
+//   getAllVectors,
+// } from "@/libs/vectorDataSetsPinecone/vectorStore";
+// import { createRouter, expressWrapper } from "next-connect";
+// const router = createRouter();
 
-router.post(async (req, res) => {
-  try {
-    var response = await callVectorDBQAChain(req.body.query);
-    res.send({
-      success: true,
-      data: response.text,
-      userSpecificLink: response.userSpecificLink,
-    });
-  } catch (error) {
-    res.send({ success: false, data: [] });
-  }
-});
+// router.post(async (req, res) => {
+//   try {
+//     var response = await callVectorDBQAChain(req.body.query);
+//     res.send({
+//       success: true,
+//       data: response.text,
+//       userSpecificLink: response.userSpecificLink,
+//     });
+//   } catch (error) {
+//     res.send({ success: false, data: [] });
+//   }
+// });
 
-export default router.handler({
-  onError: (err, req, res) => {
-    console.log(err.stack);
-    res.status(err.statusCode || 500).end(err.message);
-  },
-});
+// export default router.handler({
+//   onError: (err, req, res) => {
+//     console.log(err.stack);
+//     res.status(err.statusCode || 500).end(err.message);
+//   },
+// });
